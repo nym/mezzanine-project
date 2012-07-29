cf4da7066d5e8a5b954023b731bcf7fa8f0c82ed
Lorin Hochstein <lorin@nimbisservices.com>
2012-07-28 20:31:52 -0400

    Use DJANGO_SETTINGS_MODULE check

--- settings.py
+++ settings.py
@@ -363,9 +363,6 @@ except ImportError:
 # set_dynamic_settings() will rewrite globals based on what has been
 # defined so far, in order to provide some better defaults where
 # applicable.
-try:
+if 'DJANGO_SETTINGS_MODULE' in os.environ::
     from mezzanine.utils.conf import set_dynamic_settings
     set_dynamic_settings(globals())
-except ImportError:
-    pass
-
